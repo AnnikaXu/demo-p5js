f1ef9780a292eb06e667613867e262775a594f6f
JiheHe <nickhe2003@gmail.com>
2023-04-06 14:04:45 -0400

    Changed color to be more color blind friendly

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('#ff0000');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('#0000ff');
   }
   updateSnakeCoordinates();
   updateHandContainer();
